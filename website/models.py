f65e21e76c22492387fdc5dd5a0be9b607e88210
vishnu nayak <vishnunayak70@gmail.com>
2022-02-14 01:16:49 +0530

    Notes

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from . import db
+from flask_login import UserMixin
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password1 = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    notes = db.relationship('Note')
